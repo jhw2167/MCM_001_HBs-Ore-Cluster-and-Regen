13048f9d211ab4e17873031ed48c4b5cfacaef90
jhw2167 <jhw2167@columbia.edu>
2025-01-18 23:27:01 -0600

    - intial commit - file copy of multiloader template over - ran initializer to setup mixins and ids - does not compile

---
+++ common/src/main/java/com/holybuckets/orecluster/CommonClass.java
@@ -0,0 +1,31 @@
+package com.holybuckets.orecluster;
+
+import com.holybuckets.orecluster.platform.Services;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Items;
+
+
+public class CommonClass {
+
+    public static void init()
+    {
+
+        Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
+        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+
+        if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
+
+            Constants.LOG.info("Hello to " + Constants.MOD_NAME + "!");
+        }
+
+    }
+
+    /**
+     * Description: Run sample tests methods
+     */
+    public static void sample()
+    {
+
+    }
+
+}
---
+++ forge/src/main/java/com/holybuckets/orecluster/FoundationMain.java
@@ -0,0 +1,13 @@
+package com.holybuckets.orecluster;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod( Constants.MOD_ID)
+public class FoundationMain {
+
+    public FoundationMain() {
+        super();
+        CommonClass.init();
+    }
+
+}
