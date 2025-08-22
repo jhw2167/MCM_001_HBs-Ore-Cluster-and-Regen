c6e6e2f357fcdb35ba5ae94de6bd52ebd5f19141
jhw2167 <jhw2167@columbia.edu>
2025-08-22 11:28:18 -0500

    feat: Add client-side initialization for Forge mod with DistExecutor

--- forge/src/main/java/com/holybuckets/orecluster/OreClusterRegenMainForge.java
+++ forge/src/main/java/com/holybuckets/orecluster/OreClusterRegenMainForge.java
@@ -1,6 +1,10 @@
 package com.holybuckets.orecluster;
 
+import com.holybuckets.orecluster.client.CommonClassClient;
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.client.BalmClient;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod( Constants.MOD_ID)
